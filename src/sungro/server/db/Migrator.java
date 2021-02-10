a648a3a44d5dd22816571d9f728c7b7f93c21862
dianhenglau <dianhenglau@gmail.com>
2021-02-11 01:35:33 +0800

    Add create tables in database migration
    - Products
    - Stock
    - StockTrx
    - Sales

--- src/sungro/server/db/Migrator.java
+++ src/sungro/server/db/Migrator.java
@@ -28,96 +28,164 @@ public class Migrator {
                 m01_create_sessions(connection);
                 System.out.println("Done");
             }
+
+            if (migrations.size() < 3 || !migrations.get(2).equals("m02_create_product_stock_trx_sales")) {
+                System.out.println("Running m02_create_product_stock_trx_sales...");
+                m02_create_product_stock_trx_sales(connection);
+                System.out.println("Done");
+            }
         }
     }
 
     private ArrayList<String> getMigrations(Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
-        ResultSet row = statement.executeQuery(
-                "select 1 from sqlite_master where type = 'table' and name = 'Migrations'"
-        );
+        try (Statement statement = connection.createStatement()) {
+            try (ResultSet row = statement.executeQuery(
+                    "select 1 from sqlite_master where type = 'table' and name = 'Migrations'"
+            )) {
+                // If table "Migrations" doesn't exist
+                if (!row.next()) {
+                    statement.executeUpdate("create table Migrations (Name text primary key not null)");
 
-        // If table "Migrations" doesn't exist
-        if (!row.next()) {
-            statement.executeUpdate("create table Migrations (Name text primary key not null)");
+                    return new ArrayList<>();
+                }
+            }
 
-            return new ArrayList<>();
+            try (ResultSet row = statement.executeQuery("select Name from Migrations")) {
+                ArrayList<String> migrations = new ArrayList<>();
+                while (row.next()) {
+                    migrations.add(row.getString(1));
+                }
+
+                return migrations;
+            }
         }
-
-        row = statement.executeQuery("select Name from Migrations");
-
-        ArrayList<String> migrations = new ArrayList<>();
-        while (row.next()) {
-            migrations.add(row.getString(1));
-        }
-
-        return migrations;
     }
 
     private void m00_create_db(Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("drop table if exists Users");
 
-        statement.executeUpdate("drop table if exists Users");
+            statement.executeUpdate(
+                    "create table Users (" +
+                            "UserID integer primary key not null, " +
+                            "FirstName text not null, " +
+                            "LastName text not null, " +
+                            "Email text unique not null, " +
+                            "IDNumber text unique not null, " +
+                            "IDType text not null check (IDType in ('IC', 'Passport')), " +
+                            "Role text not null check (Role in ('Admin', 'Sales Executive')), " +
+                            "PwHash text not null, " +
+                            "ProfilePic blob not null, " +
+                            "Status text not null check (Status in ('Active', 'Inactive')), " +
+                            "CreatedBy integer references Users (UserID) on delete restrict on update restrict, " +
+                            "CreatedOn text not null" +
+                            ")"
+            );
 
-        statement.executeUpdate(
-                "create table Users (" +
-                        "UserID integer primary key not null, " +
-                        "FirstName text not null, " +
-                        "LastName text not null, " +
-                        "Email text unique not null, " +
-                        "IDNumber text unique not null, " +
-                        "IDType text not null check (IDType in ('IC', 'Passport')), " +
-                        "Role text not null check (Role in ('Admin', 'Sales Executive')), " +
-                        "PwHash text not null, " +
-                        "ProfilePic blob not null, " +
-                        "Status text not null check (Status in ('Active', 'Inactive')), " +
-                        "CreatedBy integer references Users (UserID) on delete restrict on update restrict, " +
-                        "CreatedOn text not null" +
-                        ")"
-        );
+            try (PreparedStatement preparedStatement = connection.prepareStatement(
+                    "insert into Users (" +
+                            "FirstName, LastName, Email, IDNumber, IDType, Role, PwHash, ProfilePic, Status, " +
+                            "CreatedBy, CreatedOn" +
+                            ")" +
+                            "values (" +
+                            "'Administrator', '', 'Administrator', '', 'IC', 'Admin', ?, '', 'Active', " +
+                            "1, ?" +
+                            ")"
+            )) {
+                // PwHash
+                preparedStatement.setString(1, PasswordEncoder.encode("admin"));
+                // CreatedOn
+                preparedStatement.setString(2, LocalDateTime.now().toString());
 
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "insert into Users (" +
-                        "FirstName, LastName, Email, IDNumber, IDType, Role, PwHash, ProfilePic, Status, " +
-                        "CreatedBy, CreatedOn" +
-                        ")" +
-                        "values (" +
-                        "'Administrator', '', 'Administrator', '', 'IC', 'Admin', ?, '', 'Active', " +
-                        "1, ?" +
-                        ")"
-        );
+                preparedStatement.executeUpdate();
+            }
 
-        // PwHash
-        preparedStatement.setString(1, PasswordEncoder.encode("admin"));
-        // CreatedOn
-        preparedStatement.setString(2, LocalDateTime.now().toString());
-
-        preparedStatement.executeUpdate();
-
-        statement.executeUpdate("insert into Migrations (Name) values ('m00_create_db')");
+            statement.executeUpdate("insert into Migrations (Name) values ('m00_create_db')");
+        }
     }
 
     private void m01_create_sessions(Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
+        try (Statement statement = connection.createStatement()) {
 
-        statement.executeUpdate("drop table if exists Sessions");
+            statement.executeUpdate("drop table if exists Sessions");
 
-        statement.executeUpdate(
-                "create table Sessions (" +
-                        "SessionID text primary key not null, " +
-                        "UserID integer references Users (UserID) on delete restrict on update restrict" +
-                        ")"
-        );
+            statement.executeUpdate(
+                    "create table Sessions (" +
+                            "SessionID text primary key not null, " +
+                            "UserID integer references Users (UserID) on delete restrict on update restrict" +
+                            ")"
+            );
 
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "insert into Sessions (SessionID, UserID) values (?, ?)"
-        );
+            try (PreparedStatement preparedStatement = connection.prepareStatement(
+                    "insert into Sessions (SessionID, UserID) values (?, ?)"
+            )) {
+                preparedStatement.setString(1, "0123456789abcdef");
+                preparedStatement.setInt(2, 1);
 
-        preparedStatement.setString(1, "0123456789abcdef");
-        preparedStatement.setInt(2, 1);
+                preparedStatement.executeUpdate();
+            }
 
-        preparedStatement.executeUpdate();
+            statement.executeUpdate("insert into Migrations (Name) values ('m01_create_sessions')");
+        }
+    }
 
-        statement.executeUpdate("insert into Migrations (Name) values ('m01_create_sessions')");
+    private void m02_create_product_stock_trx_sales(Connection connection) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("drop table if exists Products");
+
+            statement.executeUpdate(
+                    "create table Products (" +
+                            "ProductID integer primary key not null, " +
+                            "Name text unique not null, " +
+                            "Category text not null, " +
+                            "ProductPrice integer not null, " +
+                            "ProductPic blob not null, " +
+                            "Status text not null check (Status in ('Available', 'Disabled')), " +
+                            "CreatedBy integer references Users (UserID) on delete restrict on update restrict, " +
+                            "CreatedOn text not null" +
+                            ")"
+            );
+
+            statement.executeUpdate("drop table if exists Stock");
+
+            statement.executeUpdate(
+                    "create table Stock (" +
+                            "SKU text primary key not null, " +
+                            "ProductID integer references Products (ProductID) on delete restrict on update restrict, " +
+                            "ExpDate text not null, " +
+                            "Quantity integer not null, " +
+                            "CreatedBy integer references Users (UserID) on delete restrict on update restrict, " +
+                            "CreatedOn text not null" +
+                            ")"
+            );
+
+            statement.executeUpdate("drop table if exists StockTrx");
+
+            statement.executeUpdate(
+                    "create table StockTrx (" +
+                            "StockTrxID integer primary key not null, " +
+                            "SKU text references Stock (SKU) on delete restrict on update restrict, " +
+                            "QuantityVaried integer not null, " +
+                            "Remark text not null, " +
+                            "CreatedBy integer references Users (UserID) on delete restrict on update restrict, " +
+                            "CreatedOn text not null" +
+                            ")"
+            );
+
+            statement.executeUpdate("drop table if exists Sales");
+
+            statement.executeUpdate(
+                    "create table Sales (" +
+                            "SaleID integer primary key not null, " +
+                            "SKU text references Stock (SKU) on delete restrict on update restrict, " +
+                            "UnitPrice integer not null, " +
+                            "SoldQuantity integer not null, " +
+                            "SoldBy integer references Users (UserID) on delete restrict on update restrict, " +
+                            "SoldOn text not null" +
+                            ")"
+            );
+
+            statement.executeUpdate("insert into Migrations (Name) values ('m02_create_product_stock_trx_sales')");
+        }
     }
 }
